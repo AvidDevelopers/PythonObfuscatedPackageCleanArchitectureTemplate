e897a97ce7b1adba97eaa2ddc67adff94de9721b
Sadegh Yazdani <silverstar10@gmail.com>
2022-11-05 16:11:22 +0330

    refactor: add help to input args

--- src/obfpkg/obfuscator.py
+++ src/obfpkg/obfuscator.py
@@ -13,11 +13,13 @@ def get_args() -> argparse.ArgumentParser:
         "-s",
         "--src",
         type=PathCheck(exists=Exist(check=True), ptype=PathType.DIR),
+        help='The directory where the source package contained.'
     )
     parser.add_argument(
         "-o",
         "--obf",
         type=PathCheck(exists=Exist(empty_or_not_exist=True), ptype=PathType.DIR),
+        help='The directory where the obfuscated package will stored.'
     )
 
     return parser
