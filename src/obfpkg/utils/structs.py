06ba392a984396d5575560e7f66ab454f9440cfa
Sadegh Yazdani <silverstar10@gmail.com>
2022-11-07 17:34:48 +0330

    refactor: improve struct for PathCheck by pathlib

--- src/obfpkg/utils/structs.py
+++ src/obfpkg/utils/structs.py
@@ -1,4 +1,5 @@
-import os
+from os import listdir
+import shutil
 from pathlib import Path
 from typing import Optional, Union, Tuple, List, Callable
 from dataclasses import dataclass
@@ -16,14 +17,14 @@ class Exist:
 @dataclass
 class PathDetail:
     name: str
-    validator: Callable[[str], bool]
+    validator: Callable[[Path], bool]
 
 
 class PathType(Enum):
     # Types allowed for files.
-    FILE = PathDetail("file", os.path.isfile)
-    DIR = PathDetail("directory", os.path.isdir)
-    SYM = PathDetail("symlink", os.path.islink)
+    FILE = PathDetail("file", Path.is_dir)
+    DIR = PathDetail("directory", Path.is_dir)
+    SYM = PathDetail("symlink", Path.is_symlink)
     ALL = PathDetail("any", lambda x: True)  # Any thing is fine (don't check)
 
 
@@ -69,7 +70,8 @@ class PathCheck:
         self._dash_ok = dash_ok
 
     def __call__(self, string: str):
-        if string == "-":
+        path_item = Path(string)
+        if path_item == Path("-"):
             # the special argument "-" means sys.std[in/out]
             if self._type == PathType.DIR:
                 raise ArgumentTypeError(
@@ -85,14 +87,16 @@ class PathCheck:
 
         # If the path must exist.
         if self._exists.check:
-            if not os.path.exists(string):
+            if not path_item.exists():
                 raise ArgumentTypeError(f"path does not exist: {string}")
 
             # check PathTypes
             for label, detail in PathType.__members__.items():
                 if self._type == getattr(PathType, label):
-                    if not detail.value.validator(string):
-                        raise ArgumentTypeError(f"path is not a {detail.value.name}: {string}")
+                    if not detail.value.validator(path_item):
+                        raise ArgumentTypeError(
+                            f"path is not a {detail.value.name}: {string}"
+                        )
                     break
             else:
                 # Otherwise, call type.
@@ -103,15 +107,10 @@ class PathCheck:
                     raise ArgumentTypeError(f"path not valid: {string}")
 
         else:
-            if os.path.exists(string):
+            if path_item.exists():
                 if self._exists.empty_or_not_exist:
-                    if os.listdir(string):
-                        raise ArgumentTypeError(f"path not empty: {string}")
-
-            p = os.path.dirname(string) or os.path.curdir
-            if not os.path.exists(p):
-                raise ArgumentTypeError("parent directory does not exist: %r" % p)
-            elif not os.path.isdir(p):
-                raise ArgumentTypeError("parent path is not a directory: %r" % p)
+                    if listdir(string):
+                        shutil.rmtree(string)
+                        # raise ArgumentTypeError(f"path not empty: {string}")
 
         return Path(string)
