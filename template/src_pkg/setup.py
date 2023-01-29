c690cfd3a912b2a7377a1f2b6bc9c595dc8a9962
Sadegh Yazdani <silverstar10@gmail.com>
2023-01-29 09:55:05 +0330

    refactor: make setup.py simpler

--- template/src_pkg/setup.py
+++ template/src_pkg/setup.py
@@ -1,29 +1,12 @@
 from setuptools import find_namespace_packages, setup
 
-version = "0.1.0"
-DESCRIPTION = "A print package"
-LONG_DESCRIPTION = """A simple class for print python objects with tails."""
+version = "0.2.0"
 
 
 setup(
     name="animal",
     version=version,
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    author="Sadegh Yazdani",
-    author_email="m.s.yazdani85@gmail.com",
-    license="GPLv3",
     packages=find_namespace_packages(),
     include_package_data=True,  # Importants
-    install_requires=[],
-    keywords="printing",
-    classifiers=[
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3.8",
-    ],
     python_requires="~=3.8",
 )
